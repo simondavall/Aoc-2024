4f50681e92f8503144842e672d9576f916930c79
Simon Da Vall <simon.davall@gmail.com>
2025-01-05 15:27:32 +0000

    Aoc2024 Day4

---
+++ Day04/HelperFunctions.cs
@@ -0,0 +1,19 @@
+namespace _04;
+
+internal partial class Program
+{
+    private static List<T> List<T>(List<T> list)
+    {
+        return list;
+    }
+    
+    private static IEnumerable<int> Range(int n)
+    {
+        return Enumerable.Range(0, n);
+    }
+    
+    private static IEnumerable<int> Range(int start, int n)
+    {
+        return Enumerable.Range(start, n);
+    }
+}
--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,9 +1,6 @@
-﻿using System.Text.RegularExpressions;
-// ReSharper disable ClassNeverInstantiated.Global
+﻿namespace _04;
 
-namespace _04;
-
-internal partial class Program
+internal static partial class Program
 {
     private static string[] _map = [];
     private static int _mapWidth;
@@ -11,7 +8,7 @@ internal partial class Program
     
     internal static void Main()
     {
-        _map = File.ReadAllText("input.txt").Split("\n", StringSplitOptions.RemoveEmptyEntries);
+        _map = File.ReadAllText("input.txt").Split('\n', StringSplitOptions.RemoveEmptyEntries);
         
         _mapWidth = _map[0].Length;
         _mapHeight = _map.Length;
@@ -19,52 +16,51 @@ internal partial class Program
         Console.WriteLine($"Part 1: {PartOne()}");
         Console.WriteLine($"Part 2: {PartTwo()}");
     }
-
+    
     private static long PartOne()
     {
         long tally = 0;
-        var combinations = new []
-        {
-            _map,
-            ReverseChars(_map),
-            Down(_map),
-            ReverseChars(Down(_map)),
-            LeftDiagonal(_map),
-            ReverseChars(LeftDiagonal(_map)),
-            RightDiagonal(_map),
-            ReverseChars(RightDiagonal(_map))
-        };
         
-        foreach (var combination in combinations)
-        {
-            foreach (var line in combination)
+        for (var row = 0; row < _mapHeight - 1; row++)
+            for (var col = 0; col < _mapWidth - 1; col++)
             {
-                tally += XmasRegex().Matches(line).Count;
+                if (_map[row][col] != 'X')
+                    continue;
+                
+                foreach (var deltaRow in List([-1, 0, 1]))
+                    foreach (var deltaCol in List([-1, 0, 1]))
+                    {
+                        if (deltaRow == 0 && deltaCol == 0)
+                            continue;
+
+                        if (!IsInBounds(row, deltaRow, _map) || !IsInBounds(col, deltaCol, _map))
+                            continue;
+
+                        if (_map[row + deltaRow][col + deltaCol] == 'M'
+                            && _map[row + 2 * deltaRow][col + 2 * deltaCol] == 'A'
+                            && _map[row + 3 * deltaRow][col + 3 * deltaCol] == 'S')
+                            tally++;
+                    }
             }
-        }
-        
+
         return tally;
     }
 
     private static long PartTwo()
     {
         long tally = 0;
-
-        for (var i = 1; i < _mapHeight - 1; i++)
-        {
-            for (var j = 1; j < _mapWidth - 1; j++)
-            {
-                if (_map[i][j] == 'A' && HasX_Mas(i, j))
+        
+        foreach (var row in Range(1, _mapHeight - 2))
+            foreach (var col in Range(1, _mapWidth - 2))
+                if (_map[row][col] == 'A' && Has_Xmas(row, col))
                 {
                     tally++;
                 }
-            }
-        }
         
         return tally;
     }
 
-    private static bool HasX_Mas(int row, int col)
+    private static bool Has_Xmas(int row, int col)
     {
         return ((AboveLeft(row, col, 'M') && BelowRight(row, col, 'S'))
                 || (AboveLeft(row, col, 'S') && BelowRight(row, col, 'M')))
@@ -72,103 +68,29 @@ internal partial class Program
                    || (AboveRight(row, col, 'S') && BelowLeft(row, col, 'M')));
     }
 
-    private static bool AboveLeft(int row, int col, char c)
+    private static bool AboveLeft(int row, int col, char ch)
     {
-        return _map[row - 1][col - 1] == c;
+        return _map[row - 1][col - 1] == ch;
     }
     
-    private static bool AboveRight(int row, int col, char c)
+    private static bool AboveRight(int row, int col, char ch)
     {
-        return _map[row - 1][col + 1] == c;
+        return _map[row - 1][col + 1] == ch;
     }
     
-    private static bool BelowLeft(int row, int col, char c)
+    private static bool BelowLeft(int row, int col, char ch)
     {
-        return _map[row + 1][col - 1] == c;
+        return _map[row + 1][col - 1] == ch;
     }
     
-    private static bool BelowRight(int row, int col, char c)
+    private static bool BelowRight(int row, int col, char ch)
     {
-        return _map[row + 1][col + 1] == c;
-    }
-
-    private static string[] Down(string[] map)
-    {
-        List<string> down = [];
-        for (var i = 0; i < _mapWidth; i++)
-        {
-            var newRow = new char[_mapHeight];
-            for (var j = 0; j < _mapHeight; j++)
-            {
-                newRow[j] = map[j][i];
-            }
-            down.Add(new string(newRow));
-        }
-        return down.ToArray();
-    }
-
-    private static string[] LeftDiagonal(string[] map)
-    {
-        List<string> diagonal = [];
-        for (var i = 0; i < _mapHeight + _mapWidth; i++)
-        {
-            List<char> diagChar = [];
-            var row = i;
-            var col = 0;
-            if (i >= _mapHeight)
-            {
-                row = _mapHeight - 1;
-                col = i - _mapHeight + 1;
-            }
-
-            while (col < _mapWidth && row >= 0)
-            {
-                diagChar.Add(map[row--][col++]);
-            }
-            diagonal.Add(new string(diagChar.ToArray()));
-        }
-
-        return diagonal.ToArray();
+        return _map[row + 1][col + 1] == ch;
     }
     
-    private static string[] RightDiagonal(string[] map)
+    private static bool IsInBounds(int value, int delta, string[] map)
     {
-        List<string> diagonal = [];
-        for (var i = 0; i < _mapHeight + _mapWidth; i++)
-        {
-            List<char> diagChar = [];
-            int row; var col = _mapWidth - 1;
-            if (i >= _mapHeight)
-            {
-                row = _mapHeight - 1;
-                col = _mapWidth - 2 - (i - _mapHeight);
-            }
-            else
-            {
-                row = i;
-            }
-
-            while (col >= 0 && row >= 0)
-            {
-                diagChar.Add(map[row--][col--]);
-            }
-            diagonal.Add(new string(diagChar.ToArray()));
-        }
-
-        return diagonal.ToArray();
+        var bound = value + 3 * delta;
+        return bound >= 0 && bound < map.Length;
     }
-    
-    private static string[] ReverseChars(string[] array)
-    {
-        List<string> reverse = [];
-        foreach (var row in array)
-        {
-            reverse.Add(new string(row.Reverse().ToArray()));
-        }
-
-        return reverse.ToArray();
-    }
-
-    [GeneratedRegex("XMAS")]
-    private static partial Regex XmasRegex();
 }
